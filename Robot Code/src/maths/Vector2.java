3a97e3fdec452aa05ed592d4c3e0cc68b832dd5a
Kyle <k.leong@outlook.com>
2017-08-14 19:27:37 -0700

    Update Vector2.java
    Added an empty constructor and a copy constructor.

--- Robot Code/src/maths/Vector2.java
+++ Robot Code/src/maths/Vector2.java
@@ -19,6 +19,11 @@ public class Vector2 implements Comparable<Vector2> {
 
 	private double x;
 	private double y;
+	
+	public Vector2() {
+		this.x = 0;
+		this.y = 0;
+	}
 
 	public Vector2(double x, double y) {
 		this.x = x;
@@ -28,6 +33,11 @@ public class Vector2 implements Comparable<Vector2> {
 	public Vector2(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public Vector2(Vector2 other) {
+		this.x = other.x;
+		this.y = other.y;
 	}
 
 	public double getX() {
@@ -49,7 +59,8 @@ public class Vector2 implements Comparable<Vector2> {
 	public double length() {
 		return Math.sqrt((x * x) + (y * y));
 	}
-
+	
+	// Deprecated now that we have the copy constructor?
 	public Vector2 clone() {
 		return new Vector2(x, y);
 	}
