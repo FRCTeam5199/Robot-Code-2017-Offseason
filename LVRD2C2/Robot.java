1f553c6c6746f24b8ac9e1e426e708c835403b9d
zachFinkelstein <zachary.jasonf@gmail.com>
2017-08-25 19:55:24 -0700

    Add files via upload

---
+++ LVRD2C2/Robot.java
@@ -0,0 +1,105 @@
+package org.usfirst.frc.team5199.robot;
+
+import drive.DriveControl;
+import drive.JoystickController;
+import drive.XBoxController;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SampleRobot;
+import maths.Vector2;
+import networking.RemoteOutput;
+import networking.Vision;
+import turret.TurretControl;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class. The
+ * SampleRobot class is the base of a robot application that will automatically
+ * call your Autonomous and OperatorControl methods at the right time as
+ * controlled by the switches on the driver station or the field controls.
+ *
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SampleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ *
+ * WARNING: While it may look like a good choice to use for your code if you're
+ * inexperienced, don't. Unless you know what you are doing, complex code will
+ * be much more difficult under this system. Use IterativeRobot or Command-Based
+ * instead if you're new.
+ */
+public class Robot extends SampleRobot {
+
+	public static RemoteOutput nBroadcaster;
+	private Vision vision;
+	private XBoxController controller;
+	private JoystickController joystick;
+	private ADXRS450_Gyro gyro;
+	private Vector2 target;
+	private DriveControl driveControl;
+	private TurretControl turretControl;
+
+	public Robot() {
+
+	}
+
+	@Override
+	public void robotInit() {
+		nBroadcaster = new RemoteOutput("10.51.99.206", 1180);
+		// set first parameter in RemoteOutput constructor to your computer's
+		// local address. (ex: "10.51.99.206")
+		// currently working on getting this to work without it
+
+		gyro = new ADXRS450_Gyro();
+		Robot.nBroadcaster.println("Calibrating Gyro...");
+		gyro.calibrate();
+		Robot.nBroadcaster.println("Done!");
+
+		vision = new Vision();
+		vision.start();
+		controller = new XBoxController(0);
+		joystick = new JoystickController(1);
+		target = Vector2.ZERO.clone();
+		driveControl = new DriveControl(controller, gyro);
+		turretControl = new TurretControl(joystick, .001, 0, .002, target);
+	}
+
+	@Override
+	public void autonomous() {
+		gyro.reset();
+
+	}
+
+	@Override
+	public void operatorControl() {
+		Robot.nBroadcaster.println("\n\n\n\n\nStarting TeleOp");
+
+		gyro.reset();
+
+		while (isOperatorControl() && isEnabled()) {
+			Vector2 newTarget = vision.getPos();
+			target.setX(newTarget.getX() - 320);
+			// Robot.nBroadcaster.println(target);
+			if (newTarget.getX() == 0) {
+				target.setX(0);
+			}
+
+			driveControl.update();
+			turretControl.update();
+
+			try {
+				Thread.sleep(1000 / 100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		vision.stop();
+	}
+
+	@Override
+	public void test() {
+
+		gyro.reset();
+	}
+}
---
+++ LVRD2C2/src/org/usfirst/frc/team5199/robot/Vector2.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc.team5199.robot;
+
+
+/**
+ *
+ * @author 18wakayamats
+ */
+public class Vector2 implements Comparable<Vector2> {
+
+
+	public static final Vector2 ZERO = new Vector2(0, 0);
+	public static final Vector2 FORWARDS = new Vector2(0, 1);
+	public static final Vector2 BACKWARDS = new Vector2(0, -1);
+	public static final Vector2 LEFT = new Vector2(-1, 0);
+	public static final Vector2 RIGHT = new Vector2(1, 0);
+
+	private double x;
+	private double y;
+	
+	public Vector2() {
+		this.x = 0;
+		this.y = 0;
+	}
+
+	public Vector2(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Vector2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Vector2(Vector2 other) {
+		this.x = other.x;
+		this.y = other.y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double length() {
+		return Math.sqrt((x * x) + (y * y));
+	}
+	
+	// Deprecated now that we have the copy constructor?
+	public Vector2 clone() {
+		return new Vector2(x, y);
+	}
+
+	@Override
+	public String toString() {
+		return x + "," + y;
+	}
+
+	@Override
+	public int compareTo(Vector2 other) {
+		if (this.x == other.x) {
+			return (int) (this.y - other.y);
+		} else {
+			return (int) (this.x - other.x);
+		}
+	}
+
+	public static Vector2 add(Vector2 v1, Vector2 v2) {
+		return new Vector2(v1.x + v2.x, v1.y + v2.y);
+	}
+
+	public static Vector2 subtract(Vector2 v1, Vector2 v2) {
+		return new Vector2(v1.x - v2.x, v1.y - v2.y);
+	}
+
+}
