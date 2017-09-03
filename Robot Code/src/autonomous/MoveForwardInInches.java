be5e4b9968995bc9373ebb38781bdd27f2e69288
Kyle <zpenguin@users.noreply.github.com>
2017-09-03 13:07:59 -0700

    Updated AutFunction Comments
    And added an implementation of it.

---
+++ Robot Code/src/autonomous/MoveForwardInInches.java
@@ -0,0 +1,19 @@
+package autonomous;
+
+import interfaces.AutFunction;
+
+public class MoveForwardInInches implements AutFunction {
+
+	@Override
+	public void update(long delta) {
+		
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
--- Robot Code/src/interfaces/AutFunction.java
+++ Robot Code/src/interfaces/AutFunction.java
@@ -1,6 +1,19 @@
 package interfaces;
 
 public interface AutFunction {
-public void update(long delta);
-public boolean isDone();
+	
+	/** 
+	 * Typically one would declare a class variable called isDone and have isDone() return the boolean isDone.
+	 * Will be called forever ever 100 milliseconds (the time per update is defined in ClockRegulator) until isDone() returns true.
+	 * 
+	 * @param delta the number of milliseconds that have passed since the last time update(long) was called.
+	 */
+	public void update(long delta);
+
+	/**
+	 * Signifies to AutonomousManager whether this AutFunction is finished doing its job and no longer needs to be called.
+	 * 
+	 * @return Does the code in update(long) need to be run anymore.
+	 */
+	public boolean isDone();
 }
