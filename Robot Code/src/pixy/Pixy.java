aa89fa81f35f9f724e368f765e55957434bf7d01
HoboWithWifi <watomo00@gmail.com>
2017-09-09 16:39:47 -0700

    Fixed an import

--- Robot Code/src/pixy/Pixy.java
+++ Robot Code/src/pixy/Pixy.java
@@ -3,7 +3,7 @@ package pixy;
 import java.nio.ByteBuffer;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team5199.robot.SyncedLongBlock;
+//import org.usfirst.frc.team5199.robot.SyncedLongBlock;
 
 public class Pixy {
 
