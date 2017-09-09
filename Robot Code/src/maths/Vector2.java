517b0d279dbd9078ddabb0de43db0dcfecbb50a3
HoboWithWifi <watomo00@gmail.com>
2017-09-08 21:40:31 -0700

    Added Rotate method

--- Robot Code/src/maths/Vector2.java
+++ Robot Code/src/maths/Vector2.java
@@ -17,9 +17,11 @@ public class Vector2 implements Comparable<Vector2> {
 	public static final Vector2 LEFT = new Vector2(-1, 0);
 	public static final Vector2 RIGHT = new Vector2(1, 0);
 
+	private static final double degToRad = Math.PI / 180;
+
 	private double x;
 	private double y;
-	
+
 	public Vector2() {
 		this.x = 0;
 		this.y = 0;
@@ -34,7 +36,7 @@ public class Vector2 implements Comparable<Vector2> {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public Vector2(Vector2 other) {
 		this.x = other.x;
 		this.y = other.y;
@@ -59,7 +61,7 @@ public class Vector2 implements Comparable<Vector2> {
 	public double length() {
 		return Math.sqrt((x * x) + (y * y));
 	}
-	
+
 	// Deprecated now that we have the copy constructor?
 	public Vector2 clone() {
 		return new Vector2(x, y);
@@ -86,11 +88,11 @@ public class Vector2 implements Comparable<Vector2> {
 	public static Vector2 subtract(Vector2 v1, Vector2 v2) {
 		return new Vector2(v1.x - v2.x, v1.y - v2.y);
 	}
-	
+
 	public static Vector2 multiply(Vector2 v1, Vector2 v2) {
 		return new Vector2(v1.x * v2.x, v1.y * v2.y);
 	}
-	
+
 	public static Vector2 multiply(Vector2 v1, double n) {
 		return new Vector2(v1.x * n, v1.y * n);
 	}
@@ -98,5 +100,25 @@ public class Vector2 implements Comparable<Vector2> {
 	public static Vector2 divide(Vector2 v1, Vector2 v2) {
 		return new Vector2(v1.x / v2.x, v1.y / v2.y);
 	}
+
+	public static Vector2 rotate(Vector2 v, double d) {
+		double sin = Math.sin(d);
+		double cos = Math.cos(d);
+		return new Vector2(v.getX() * cos - v.getY() * sin, v.getX() * sin + v.getY() * cos);
+	}
+	
+	public static Vector2 rotateCW(Vector2 v, double d) {
+		return rotate(v, -d);
+	}
+
+	public static Vector2 rotateDeg(Vector2 v, double d) {
+		return rotate(v, d * degToRad);
+	}
+	
+	public static Vector2 rotateDegCW(Vector2 v, double d) {
+		return rotateDeg(v, -d);
+	}
+
+	
 
 }
