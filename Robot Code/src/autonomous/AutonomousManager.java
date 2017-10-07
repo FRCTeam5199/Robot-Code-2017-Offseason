486277ead6c238163984522ef2b516adc30b42d5
HoboWithWifi <watomo00@gmail.com>
2017-10-07 01:12:15 -0700

    Fixed step transition bug that caused motors to keep running

--- Robot Code/src/autonomous/AutonomousManager.java
+++ Robot Code/src/autonomous/AutonomousManager.java
@@ -4,24 +4,36 @@ import java.util.ArrayList;
 
 import org.usfirst.frc.team5199.robot.Robot;
 
+import climber.Climber;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import intake.Intake;
 import interfaces.AutFunction;
-import util.ClockRegulator;;
+import turret.Turret;
+import util.ClockRegulator;
+import drive.DriveBase;
 
 public class AutonomousManager {
 	private final ArrayList<AutFunction> functions;
 	private final ClockRegulator clockRegulator;
+
+	private final Stop stop;
+
 	private int step;
 	private boolean done;
 
-	public AutonomousManager(ClockRegulator clockRegulator) {
+	public AutonomousManager(DriveBase base, Turret turret, Intake intake, Climber climber,
+			ClockRegulator clockRegulator) {
 		this.clockRegulator = clockRegulator;
+
+		stop = new Stop(base, turret, intake, climber);
+
 		functions = new ArrayList<AutFunction>();
 		step = 0;
 	}
 
 	public void init() {
 		functions.get(0).init();
+		stop.update(1);
 	}
 
 	public void update() {
@@ -29,6 +41,7 @@ public class AutonomousManager {
 			functions.get(step).update(clockRegulator.getMsPerUpdate());
 			if (functions.get(step).isDone()) {
 				Robot.nBroadcaster.println(functions.get(step).getClass().getName() + " end\t");
+				stop.update(1);
 				step++;
 				if (step < functions.size()) {
 					functions.get(step).init();
@@ -37,6 +50,7 @@ public class AutonomousManager {
 		} else {
 			if (!done) {
 				Robot.nBroadcaster.println("AutonomousManager finished");
+				stop.update(1);
 				done = true;
 			}
 		}
@@ -46,8 +60,8 @@ public class AutonomousManager {
 	public void add(AutFunction f) {
 		functions.add(f);
 	}
-	
-	public boolean isDone(){
+
+	public boolean isDone() {
 		return done;
 	}
 
