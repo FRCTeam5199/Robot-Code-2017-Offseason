7d72ef05a3ddcac2ebc2b69568e81fdb4614aa14
HoboWithWifi <watomo00@gmail.com>
2017-10-08 20:35:15 -0700

    Added different offsets for different pegs

--- Robot Code/src/autonomous/ShootPrep.java
+++ Robot Code/src/autonomous/ShootPrep.java
@@ -1,5 +1,7 @@
 package autonomous;
 
+import org.usfirst.frc.team5199.robot.Robot;
+
 import interfaces.AutFunction;
 
 import turret.Turret;
@@ -10,11 +12,14 @@ public class ShootPrep implements AutFunction {
 	private Turret turret;
 	private double rpm;
 	private double rpmMargin = 10;
-	private double aimMargin = 5;
+	private double aimMargin = 3;
+	private double rateMargin = 1;
 
-	public ShootPrep(TurretControl turretControl, double rpm, Turret turret) {
+	public ShootPrep(TurretControl turretControl, Turret turret, double rpm, int offset) {
 		this.turretControl = turretControl;
 		this.turret = turret;
+		this.rpm = rpm;
+		turretControl.setOffset(offset);
 	}
 
 	@Override
@@ -31,7 +36,9 @@ public class ShootPrep implements AutFunction {
 
 	@Override
 	public boolean isDone() {
-		return Math.abs(turret.getFlyWheelRPM() - rpm) < rpmMargin && Math.abs(turretControl.getError()) < aimMargin;
+		Robot.nBroadcaster.println(turret.getFlyWheelRPM() - rpm + "\t" + turretControl.getError());
+		return Math.abs(turret.getFlyWheelRPM() - rpm) < rpmMargin && Math.abs(turretControl.getError()) < aimMargin
+				&& Math.abs(turretControl.getLockErrorRate()) < rateMargin;
 	}
 
 }
